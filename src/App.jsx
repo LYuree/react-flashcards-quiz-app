6c6f418af2bbbe7c41d67568252ce41d9e12498c
LYuree <slymerrow@gmail.com>
2024-09-28 22:03:57 +0700

    Implemented generating chosen amount of questions by selected category

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import axios from 'axios';
 import './app.css'
 
 export default function App() {
-  // const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
   const [flashcards, setFlashcards] = useState([]);
   const [categories, setCategories] = useState([]);
   const categoryEl = useRef();
+  const amountEl = useRef(); //usr snippet
 
   useEffect(() => {
     axios
@@ -18,9 +18,41 @@ export default function App() {
       })
     },[]);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   axios
+  //   .get('https://opentdb.com/api.php?amount=10')
+  //   .then(res => {
+  //     // console.log(res.data); // yeah, actually 10
+  //     setFlashcards(res.data.results.map((questionItem, index) => {
+  //       const answer = questionItem.correct_answer
+  //       const options = [...questionItem.incorrect_answers, answer];
+  //       return {
+  //         id: `${index}-${Date.now}`,
+  //         question: decodeString(questionItem.question),
+  //         answer: questionItem.correct_answer,
+  //         options: options.sort(() => Math.random() - .5)
+  //       }
+  //     }))
+  //   });
+  // }, []);
+
+  function decodeString(str){
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;    
+    // won't work :(
+    // const textArea = <textarea>{str}</textarea>
+    return textArea.value;
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
     axios
-    .get('https://opentdb.com/api.php?amount=10')
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
     .then(res => {
       // console.log(res.data); // yeah, actually 10
       setFlashcards(res.data.results.map((questionItem, index) => {
@@ -34,18 +66,6 @@ export default function App() {
         }
       }))
     });
-  }, []);
-
-  function decodeString(str){
-    const textArea = document.createElement('textarea');
-    textArea.innerHTML = str;    
-    // won't work :(
-    // const textArea = <textarea>{str}</textarea>
-    return textArea.value;
-  }
-
-  function handleSubmit(e){
-    e.preventDefault();
   }
 
 
@@ -59,11 +79,15 @@ export default function App() {
             return <option value={category.id} key={category.id}>{category.name}</option>
           })}
         </select>
-        <label htmlFor=""></label>
-        <input type="number" name="" id="" />
-        <button type="submit"></button>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of questions</label>
+          <input type="number" name="" id="amount" min="1" max="50" step="1" defaultValue={10} ref={amountEl}/>
+          <button className="btn">Generate</button>
+        </div>
+        <div className="form-group">
+        </div>
         
-      </div>
     </form>
       <div className="container">
         <FlashcardList flashcards={flashcards}></FlashcardList>
